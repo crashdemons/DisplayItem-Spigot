f945adae378481c11c041c656b3d73ddc9ec5bf4
crashdemons <crashenator@gmail.com>
2019-11-29 15:06:56 -0600

    test SplitChatMessage, prepare util class

---
+++ src/main/java/com/github/crashdemons/displayitem_spigot/TextUtils.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.crashdemons.displayitem_spigot;
+
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.ChatColor;
+
+/**
+ *
+ * @author crashdemons (crashenator at gmail.com)
+ */
+public class TextUtils {
+    private TextUtils(){}
+    public static String toLegacyText(BaseComponent[] components, boolean format){
+        String legacyMessage = "";
+        for(BaseComponent component : components){
+            legacyMessage+=component.toLegacyText();
+        }
+        if(!format) return ChatColor.stripColor(legacyMessage);
+        return legacyMessage;
+    }
+}
---
+++ src/test/java/com/github/crashdemons/displayitem_spigot/SplitChatMessageTest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.crashdemons.displayitem_spigot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author crashdemons (crashenator at gmail.com)
+ */
+public class SplitChatMessageTest {
+
+    public SplitChatMessageTest() {
+    }
+
+    protected void assertSplitValues(SplitChatMessage msg, String prefix, String content, String suffix) {
+        assertEquals(TextUtils.toLegacyText(msg.prefix,false), prefix);
+        assertEquals(TextUtils.toLegacyText(msg.content,false), content);
+        assertEquals(TextUtils.toLegacyText(msg.suffix,false), suffix);
+    }
+
+    @Test
+    public void testFrom() {
+        System.out.println("from");
+
+        SplitChatMessage result;
+
+        result = SplitChatMessage.from("[i]message", "[i]");
+        assertSplitValues(result, "", "[i]", "message");
+
+        result = SplitChatMessage.from("mes[i]sage", "[i]");
+        assertSplitValues(result, "mes", "[i]", "sage");
+
+        result = SplitChatMessage.from("message[i]", "[i]");
+        assertSplitValues(result, "message", "[i]", "");
+
+        try {
+            SplitChatMessage.from("message", "[i]");
+            fail("the expected exception did not occur: IllegalArgumentException; none");
+        } catch (Exception e) {
+            if (!(e instanceof IllegalArgumentException)) {
+                fail("the expected exception did not occur: IllegalArgumentException; "+e.getClass().getName());
+            }
+        }
+
+    }
+    /*
+    @Test
+    public void testFromWithExternalReplacement() {
+        System.out.println("fromWithExternalReplacement");
+        String message = "";
+        String elementString = "";
+        String externalReplacement = "";
+        String externalReplaceWith = "";
+        SplitChatMessage expResult = null;
+        SplitChatMessage result = SplitChatMessage.fromWithExternalReplacement(message, elementString, externalReplacement, externalReplaceWith);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testToComponents() {
+        System.out.println("toComponents");
+        SplitChatMessage instance = new SplitChatMessage();
+        BaseComponent[] expResult = null;
+        BaseComponent[] result = instance.toComponents();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+     */
+
+}
