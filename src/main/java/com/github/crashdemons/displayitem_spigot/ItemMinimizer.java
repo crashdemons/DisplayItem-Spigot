1c87701f9178b94cc71942e60e1b1fbca4fc67a3
crashdemons <crashenator@gmail.com>
2020-07-22 18:57:05 -0500

    placeholder class

---
+++ src/main/java/com/github/crashdemons/displayitem_spigot/ItemMinimizer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.crashdemons.displayitem_spigot;
+
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author crashdemons (crashenator at gmail.com)
+ */
+public class ItemMinimizer {
+    private ItemMinimizer(){}
+    
+    
+    public static ItemStack minimize(ItemStack stack){
+        ItemStack copiedItem = new ItemStack(stack);
+        
+        
+    }
+}
