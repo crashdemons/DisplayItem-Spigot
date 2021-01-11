81e1cb64d10277182fc674df176431d1786a2991
crashdemons <crashenator@gmail.com>
2021-01-10 19:57:38 -0600

    format source [CI-SKIP]

--- src/main/java/com/github/crashdemons/displayitem_spigot/ItemNameTranslationHelper.java
+++ src/main/java/com/github/crashdemons/displayitem_spigot/ItemNameTranslationHelper.java
@@ -14,9 +14,11 @@ import org.bukkit.inventory.ItemStack;
  * @author crashdemons (crashenator at gmail.com)
  */
 public class ItemNameTranslationHelper {
-    private ItemNameTranslationHelper(){}
-    
-    public static boolean hasClass(String classname){
+
+    private ItemNameTranslationHelper() {
+    }
+
+    public static boolean hasClass(String classname) {
         try {
             Class<?> providerClass = Class.forName(classname);
             return true;
@@ -24,24 +26,23 @@ public class ItemNameTranslationHelper {
             return false;
         }
     }
-    
-    public static String getMaterialName(ItemStack item){
+
+    public static String getMaterialName(ItemStack item) {
         return getMaterialName(item.getType());
     }
-    public static String getMaterialName(Material mat){
-        if(hasClass("net.md_5.bungee.chat.TranslationRegistry")){
-        
-            try{
+
+    public static String getMaterialName(Material mat) {
+        if (hasClass("net.md_5.bungee.chat.TranslationRegistry")) {
+            try {
                 String matName = mat.name().toLowerCase();//NOTE: bukkit name may not always match internal minecraft name!
-                String trans = TranslationRegistry.INSTANCE.translate( "item.minecraft."+matName );
+                String trans = TranslationRegistry.INSTANCE.translate("item.minecraft." + matName);
                 return trans;
-            }catch(Exception e){
+            } catch (Exception e) {
                 return null;
             }
-
             //BaseComponent[] components = ComponentSerializer.parse("{\"translate\":\"block.minecraft."+matName+"\"}");
             //return BaseComponent.toPlainText(components);
-        }else{
+        } else {
             //DisplayItem.plugin.getLogger().warning("BungeeChat TranslationRegistry support missing! please update your server!");
             return null;
         }
